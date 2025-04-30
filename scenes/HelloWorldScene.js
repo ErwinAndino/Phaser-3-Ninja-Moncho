d8bd8309f4b724927c630edb4548b953d89a0b42
ErwinAndino <erwin.andino@hotmail.com>
2025-04-29 21:27:26 -0300

    se creo el juego base, en donde recolectar 2 de cada collectable otorga la victoria

--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -16,31 +16,122 @@ export default class HelloWorldScene extends Phaser.Scene {
 
   preload() {
     // load assets
-    this.load.image("sky", "./assets/space3.png");
-    this.load.image("logo", "./assets/phaser3-logo.png");
-    this.load.image("red", "./assets/particles/red.png");
+    this.load.image("sky", "./public/assets/Cielo.webp");
+    this.load.image("background", "./public/assets/FondoMenu.jpg");
+    this.load.image("ninja", "./public/assets/Ninja.png");
+    this.load.image("platform", "./public/assets/platform.png");
+    this.load.image("diamond", "./public/assets/diamond.png");
+    this.load.image("square", "./public/assets/square.png");
+    this.load.image("triangle", "./public/assets/triangle.png");
   }
 
   create() {
     // create game objects
-    this.add.image(400, 300, "sky");
+    this.add.image(400, 300, "sky").setScale(2);
 
-    const logo = this.physics.add.image(400, 100, "logo");
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    this.platforms = this.physics.add.staticGroup();
+ 
+    this.platforms.create(400, 568, "platform").setScale(2).refreshBody();
 
-    // emmit particles from logo
-    const emitter = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
+    this.player = this.physics.add.image(400, 100, "ninja").setScale(0.1);
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+  this.collectable = this.physics.add.group();
+
+    this.totalscore = 0;
+    this.totalcollectable = [0, 0, 0];
+
+    this.gameover = 0;
+
+    this.scoreText = this.add.text(16, 16, `Score: ${this.totalscore}`, {
+      fontSize: "32px",
+      fill: "#fff",
     });
 
-    emitter.startFollow(logo);
+    this.physics.add.collider(this.player, this.platforms);
+
+    this.physics.add.collider(this.collectable, this.platforms);
+ 
+    this.physics.add.overlap(
+      this.player,
+      this.collectable,
+      this.collectablegrabbed,
+      null,
+      this
+    );
+
+    this.collectableevent = this.time.addEvent({
+      delay: 1000,
+      callback: this.spawnCollectable,
+      callbackScope: this,
+     
+      loop: true,
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+
   }
 
   update() {
     // update game objects
+
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+    } else {
+      this.player.setVelocityX(0);
+    }
+  if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+
+    if (this.totalcollectable.every(num => num >= 2)) {
+      this.scoreText.setText(`Game Over! Score: ${this.totalscore}`);
+    }
+
+}
+
+spawnCollectable() {
+
+var x = Phaser.Math.Between(1, 3);
+  var type = "";
+  if (x == 1) {
+    type = "triangle";
   }
+  if (x == 2) {
+    type = "square";
+  } 
+  if (x == 3) {
+    type = "diamond";
+  }
+
+var collectable = this.collectable.create(  Phaser.Math.Between(0, 800), 16, type).setScale(1);
+collectable.setBounce(1);
+collectable.setCollideWorldBounds(true);
+collectable.setVelocity(0 , 20);
+}
+
+collectablegrabbed(player, collectable) {
+  collectable.disableBody(true, true);
+
+  if (collectable.texture.key == "triangle") {
+    this.totalscore += 10;
+    this.totalcollectable[0] += 1;
+  }
+  if (collectable.texture.key == "square") {
+    this.totalscore += 20;
+    this.totalcollectable[1] += 1;
+  }
+  if (collectable.texture.key == "diamond") {
+    this.totalscore += 30;
+    this.totalcollectable[2] += 1;
+  }
+  this.scoreText.setText(`Score: ${this.totalscore}`);
+
+}
+
 }
